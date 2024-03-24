a60638c60410b09c762eb171d67b520f4f05dfec
Pavel <60060559+pyrogn@users.noreply.github.com>
2024-03-24 22:05:48 +0300

    Set correct author

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="map-boxes",
     version="1.0.6",
     description="Function to calculate mAP for set of detected boxes and annotated boxes.",
-    author="123",
+    author="Roman Solovyev, IPPM RAS, modified for new numpy by me",
     packages=find_packages(include=["map_boxes", "map_boxes.*"]),
     install_requires=["pandas", "numpy", "cython"],
     package_data={"map_boxes": ["compute_overlap.pyx"]},
