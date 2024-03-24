97284d8f3bde4ce70ee9dbb273d7f182a99d25e1
Pavel <60060559+pyrogn@users.noreply.github.com>
2024-03-24 21:59:07 +0300

    Add cython, will it work?

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='map-boxes',
-    version='1.0.6',
-    description='Function to calculate mAP for set of detected boxes and annotated boxes.',
-    author='123',
-    packages=find_packages(include=['map_boxes', 'map_boxes.*']),
-    install_requires=[
-		'pandas',
-		'numpy',
-    ]
+    name="map-boxes",
+    version="1.0.6",
+    description="Function to calculate mAP for set of detected boxes and annotated boxes.",
+    author="123",
+    packages=find_packages(include=["map_boxes", "map_boxes.*"]),
+    install_requires=["pandas", "numpy", "cython"],
 )
